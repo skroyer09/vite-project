bd79f48875e0bb8372d9803d691703d7fedc9ae1
steffen kroyer <stef5450@stud.kea.dk>
2024-09-10 16:38:36 +0200

    Add axios dependency and implement GameGrid component

---
+++ src/components/GameGrid.tsx
@@ -0,0 +1,18 @@
+import useGames from "../hooks/useGames";
+
+
+const GameGrid = () => {
+  const { games, error } = useGames();
+  return (
+    <div>
+      {error && <p>{error}</p>}
+      <ul>
+        {games.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default GameGrid;
