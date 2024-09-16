ba1209d46c6715cb5b294b2b28ee0c95a0b1505b
steffen kroyer <stef5450@stud.kea.dk>
2024-09-16 10:13:26 +0200

    Add Chakra UI Icons and React Icons dependencies

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,16 +1,13 @@
-import { HStack, Switch, Text, useColorMode } from '@chakra-ui/react';
+import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
 
 export const ColorModeSwitch = () => {
-    const { colorMode, toggleColorMode } = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <HStack>
-        <Switch 
-            onChange={toggleColorMode}
-            colorScheme="green"
-            isChecked = {colorMode === "dark"}/>
-        <Text>Dark Mode</Text>
+      <Switch onChange={toggleColorMode} colorScheme="green" isChecked={colorMode === "dark"} />
+      <Text>Dark Mode</Text>
     </HStack>
-  )
+  );
 };
 
 export default ColorModeSwitch;
